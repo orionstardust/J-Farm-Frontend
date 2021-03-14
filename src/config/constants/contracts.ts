cfcb3749d831f859ddc950f3a1d9dd8aab054269
orionstardust <justinono1990@outlook.com>
2021-03-14 20:50:19 +0900

    Update contracts.ts

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,22 +1,22 @@
 export default {
   bscpad:{
-    56: '0x5a3010d4d8d3b5fb49f8b6e57fb9e48063f16700',
+    56: '',
     97: '0x5a3010d4d8d3b5fb49f8b6e57fb9e48063f16700',
   },
   sil: {
-    56: '0xc66E4De0d9b4F3CB3f271c37991fE62f154471EB',
+    56: '',
     97: '0xc66E4De0d9b4F3CB3f271c37991fE62f154471EB',
   },
   cake: {
-    56: '0xCe884E60dD45d578B03275D94D546622eDf55978',
-    97: '0xDE7FD400F6091c5786271A3aA6E7587Ae60390D6',
+    56: '',
+    97: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
   },
   masterChef: {
-    56: '0x97e5Cff233a019e9050942768749Caec49Fc3e64',
-    97: '0xa5f8061Daf38E813dB352069560FCAeC061594f9',
+    56: '',
+    97: '0x318c3AE7423Cc82F9EC524353Ec2526f72D70386',
   },
   wbnb: {
-    56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+    56: '',
     97: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
   },
   lottery: {
@@ -28,15 +28,15 @@ export default {
     97: '',
   },
   mulltiCall: {
-    56: '0x1ee38d535d541c55c9dae27b12edf090c608e6fb',
+    56: '',
     97: '0x2b4b36861769060c9B3ff8199d2343F731db240D',
   },
   busd: {
-    56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-    97: '0xed24fc36d5ee211ea25a80239fb8c4cfd80f12ee',
+    56: '',
+    97: '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56',
   },
-  eth: {
-    56: '0x2170ed0880ac9a755fd29b2688956bd959f933f8',
-    97: '0xed24fc36d5ee211ea25a80239fb8c4cfd80f12ee', // FIXME:
+  dino: {
+    56: '',
+    97: '0x9510b145010f0eAc5a3aefd9a5A07A0a14B2241d', // FIXME:
   },
 }
