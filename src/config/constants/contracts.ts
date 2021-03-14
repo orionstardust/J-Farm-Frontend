0fa7130788a1c1911eccb8c17d3c3428c6ea2b76
orionstardust <justinono1990@outlook.com>
2021-03-14 16:02:57 +0300

    Update contracts.ts 56 same as 97

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,42 +1,42 @@
 export default {
   bscpad:{
-    56: '',
-    97: '0x5a3010d4d8d3b5fb49f8b6e57fb9e48063f16700',
+
+    97: '0x5a3010d4d8d3b5fb49f8b6e57fb9e48063f16700', 56: '0x5a3010d4d8d3b5fb49f8b6e57fb9e48063f16700',
   },
   sil: {
-    56: '',
-    97: '0xc66E4De0d9b4F3CB3f271c37991fE62f154471EB',
+
+    97: '0xc66E4De0d9b4F3CB3f271c37991fE62f154471EB', 56: '0xc66E4De0d9b4F3CB3f271c37991fE62f154471EB',
   },
   cake: {
-    56: '',
-    97: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
+
+    97: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82', 56: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
   },
   masterChef: {
-    56: '',
-    97: '0x318c3AE7423Cc82F9EC524353Ec2526f72D70386',
+
+    97: '0x318c3AE7423Cc82F9EC524353Ec2526f72D70386', 56: '0x318c3AE7423Cc82F9EC524353Ec2526f72D70386',
   },
   wbnb: {
-    56: '',
-    97: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+
+    97: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', 56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
   },
   lottery: {
-    56: '',
-    97: '',
+
+    97: '', 56: '',
   },
   lotteryNFT: {
-    56: '',
-    97: '',
+
+    97: '', 56: '',
   },
   mulltiCall: {
-    56: '',
-    97: '0x2b4b36861769060c9B3ff8199d2343F731db240D',
+
+    97: '0x2b4b36861769060c9B3ff8199d2343F731db240D', 56: '0x2b4b36861769060c9B3ff8199d2343F731db240D',
   },
   busd: {
-    56: '',
-    97: '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56',
+
+    97: '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56', 56: '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56',
   },
   dino: {
-    56: '',
-    97: '0x9510b145010f0eAc5a3aefd9a5A07A0a14B2241d', // FIXME:
+
+    97: '0x9510b145010f0eAc5a3aefd9a5A07A0a14B2241d', 56: '0x9510b145010f0eAc5a3aefd9a5A07A0a14B2241d', // FIXME:
   },
 }
--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -9,13 +9,12 @@ const farms: FarmConfig[] = [
     isTokenOnly: true,
     lpSymbol: 'DINO',
     lpAddresses: {
-      97: '0x9510b145010f0eAc5a3aefd9a5A07A0a14B2241d',
-      56: '', // DINO
+      97: '0x9510b145010f0eAc5a3aefd9a5A07A0a14B2241d', 56: '0x9510b145010f0eAc5a3aefd9a5A07A0a14B2241d',
+       // DINO
     },
     tokenSymbol: 'DINO',
     tokenAddresses: {
-      97: '0x9510b145010f0eAc5a3aefd9a5A07A0a14B2241d',
-      56: '',
+      97: '0x9510b145010f0eAc5a3aefd9a5A07A0a14B2241d', 56: '0x9510b145010f0eAc5a3aefd9a5A07A0a14B2241d',
     },
     quoteTokenSymbol: QuoteToken.DINO,
     quoteTokenAdresses: contracts.dino,
@@ -26,13 +25,13 @@ const farms: FarmConfig[] = [
     risk: 2,
     lpSymbol: 'DINO-BUSD',
     lpAddresses: {
-      97: '0xa0d2502Ac6d862596325Cd1B724589fe14902D99',
-      56: '',
+      97: '0xa0d2502Ac6d862596325Cd1B724589fe14902D99', 56: '0xa0d2502Ac6d862596325Cd1B724589fe14902D99',
+      
     },
     tokenSymbol: 'DINO',
     tokenAddresses: {
-      97: '0xa0d2502Ac6d862596325Cd1B724589fe14902D99',
-      56: '',
+      97: '0xa0d2502Ac6d862596325Cd1B724589fe14902D99', 56: '0xa0d2502Ac6d862596325Cd1B724589fe14902D99',
+      
     },
     quoteTokenSymbol: QuoteToken.DINO,
     quoteTokenAdresses: contracts.dino,
@@ -43,13 +42,13 @@ const farms: FarmConfig[] = [
     risk: 2,
     lpSymbol: 'DINO-BNB',
     lpAddresses: {
-      97: '0xE6BDEa4578329b908461EB76A6705fa18b22a1C6', // FIXME:
-      56: '',
+      97: '0xE6BDEa4578329b908461EB76A6705fa18b22a1C6', 56: '0xE6BDEa4578329b908461EB76A6705fa18b22a1C6', // FIXME:
+      
     },
     tokenSymbol: 'DINO',
     tokenAddresses: {
-      97: '0xE6BDEa4578329b908461EB76A6705fa18b22a1C6',
-      56: '',
+      97: '0xE6BDEa4578329b908461EB76A6705fa18b22a1C6', 56: '0xE6BDEa4578329b908461EB76A6705fa18b22a1C6',
+      
     },
     quoteTokenSymbol: QuoteToken.DINO,
     quoteTokenAdresses: contracts.dino,
@@ -60,13 +59,13 @@ const farms: FarmConfig[] = [
     risk: 1,
     lpSymbol: 'SIL-BNB',
     lpAddresses: {
-      97: '0x51509ef5ed3fa020b6011ab10e0383e089fd4823', // FIXME:
-      56: '',
+      97: '0x51509ef5ed3fa020b6011ab10e0383e089fd4823', 56: '0x51509ef5ed3fa020b6011ab10e0383e089fd4823', // FIXME:
+      
     },
     tokenSymbol: 'SIL',
     tokenAddresses: {
-      97: '0x51509ef5ed3fa020b6011ab10e0383e089fd4823', // FIXME:
-      56: '', // https://bscscan.com/address/0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c
+      97: '0x51509ef5ed3fa020b6011ab10e0383e089fd4823', 56: '0x51509ef5ed3fa020b6011ab10e0383e089fd4823', // FIXME:
+       // https://bscscan.com/address/0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c
     },
     quoteTokenSymbol: QuoteToken.SIL,
     quoteTokenAdresses: contracts.sil, // FIXME: use contracts.bnb instead
@@ -77,13 +76,13 @@ const farms: FarmConfig[] = [
     risk: 1,
     lpSymbol: 'BSCPAD-BNB',
     lpAddresses: {
-      97: '0x1e47da535d9e96e2f9348d00c18fbe967566bc28', // FIXME:
-      56: '',
+      97: '0x1e47da535d9e96e2f9348d00c18fbe967566bc28', 56: '0x1e47da535d9e96e2f9348d00c18fbe967566bc28', // FIXME:
+      
     },
     tokenSymbol: 'BSCPAD',
     tokenAddresses: {
-      97: '0x1e47da535d9e96e2f9348d00c18fbe967566bc28', // FIXME:
-      56: '', // https://bscscan.com/address/0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c
+      97: '0x1e47da535d9e96e2f9348d00c18fbe967566bc28', 56: '0x1e47da535d9e96e2f9348d00c18fbe967566bc28', // FIXME:
+       // https://bscscan.com/address/0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c
     },
     quoteTokenSymbol: QuoteToken.CAKE,
     quoteTokenAdresses: contracts.cake, // FIXME: use contracts.bnb instead
@@ -94,13 +93,13 @@ const farms: FarmConfig[] = [
     risk: 1,
     lpSymbol: 'BUSD-BNB',
     lpAddresses: {
-      97: '0x1b96b92314c44b159149f7e0303511fb2fc4774f', // FIXME:
-      56: '',
+      97: '0x1b96b92314c44b159149f7e0303511fb2fc4774f', 56: '0x1b96b92314c44b159149f7e0303511fb2fc4774f', // FIXME:
+      
     },
     tokenSymbol: 'BUSD',
     tokenAddresses: {
-      97: '0x1b96b92314c44b159149f7e0303511fb2fc4774f', // FIXME:
-      56: '', // https://bscscan.com/address/0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3
+      97: '0x1b96b92314c44b159149f7e0303511fb2fc4774f', 56: '0x1b96b92314c44b159149f7e0303511fb2fc4774f', // FIXME:
+       // https://bscscan.com/address/0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3
     },
     quoteTokenSymbol: QuoteToken.BUSD,
     quoteTokenAdresses: contracts.busd, // FIXME: use contracts.bnb instead
@@ -111,13 +110,13 @@ const farms: FarmConfig[] = [
     risk: 1,
     lpSymbol: 'CAKE-BNB',
     lpAddresses: {
-      97: '0xa527a61703d82139f8a06bc30097cc9caa2df5a6', // FIXME:
-      56: '', // https://bscscan.com/address/0x99d865Ed50D2C32c1493896810FA386c1Ce81D91
+      97: '0xa527a61703d82139f8a06bc30097cc9caa2df5a6', 56: '0xa527a61703d82139f8a06bc30097cc9caa2df5a6', // FIXME:
+       // https://bscscan.com/address/0x99d865Ed50D2C32c1493896810FA386c1Ce81D91
     },
     tokenSymbol: 'CAKE',
     tokenAddresses: {
-      97: '0xa527a61703d82139f8a06bc30097cc9caa2df5a6', // FIXME:
-      56: '', // https://bscscan.com/address/0x2170ed0880ac9a755fd29b2688956bd959f933f8
+      97: '0xa527a61703d82139f8a06bc30097cc9caa2df5a6', 56: '0xa527a61703d82139f8a06bc30097cc9caa2df5a6', // FIXME:
+       // https://bscscan.com/address/0x2170ed0880ac9a755fd29b2688956bd959f933f8
     },
     quoteTokenSymbol: QuoteToken.BNB,
     quoteTokenAdresses: contracts.wbnb, // FIXME: use contracts.bnb instead
@@ -128,13 +127,13 @@ const farms: FarmConfig[] = [
     risk: 1,
     lpSymbol: 'USDT-BUSD',
     lpAddresses: {
-      97: '0xc15fa3E22c912A276550F3E5FE3b0Deb87B55aCd', // FIXME:
-      56: '', // https://bscscan.com/address/0x0ed8e0a2d99643e1e65cca22ed4424090b8b7458
+      97: '0xc15fa3E22c912A276550F3E5FE3b0Deb87B55aCd', 56: '0xc15fa3E22c912A276550F3E5FE3b0Deb87B55aCd', // FIXME:
+       // https://bscscan.com/address/0x0ed8e0a2d99643e1e65cca22ed4424090b8b7458
     },
     tokenSymbol: 'USDT',
     tokenAddresses: {
-      97: '0xc15fa3E22c912A276550F3E5FE3b0Deb87B55aCd', // FIXME:
-      56: '', // https://bscscan.com/address/0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82
+      97: '0xc15fa3E22c912A276550F3E5FE3b0Deb87B55aCd', 56: '0xc15fa3E22c912A276550F3E5FE3b0Deb87B55aCd', // FIXME:
+       // https://bscscan.com/address/0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82
     },
     quoteTokenSymbol: QuoteToken.BUSD,
     quoteTokenAdresses: contracts.busd,
@@ -144,12 +143,12 @@ const farms: FarmConfig[] = [
   //   risk: 1,
   //   lpSymbol: 'CAKE-BNB',
   //   lpAddresses: {
-  //     97: '0xDcE45b2dc62239DD09D6ED97Eefb9276C634602c', // FIXME:
+  //     97: '0xDcE45b2dc62239DD09D6ED97Eefb9276C634602c', 56: '0xDcE45b2dc62239DD09D6ED97Eefb9276C634602c', // FIXME:
   //     56: '0xa527a61703d82139f8a06bc30097cc9caa2df5a6', // https://bscscan.com/address/0xa527a61703d82139f8a06bc30097cc9caa2df5a6
   //   },
   //   tokenSymbol: 'CAKE',
   //   tokenAddresses: {
-  //     97: '0x89dcddca577f3658a451775d58ea99da532263c8', // FIXME:
+  //     97: '0x89dcddca577f3658a451775d58ea99da532263c8', 56: '0x89dcddca577f3658a451775d58ea99da532263c8', // FIXME:
   //     56: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82', // https://bscscan.com/address/0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82
   //   },
   //   quoteTokenSymbol: QuoteToken.CAKE,
@@ -162,13 +161,13 @@ const farms: FarmConfig[] = [
     isTokenOnly: true,
     lpSymbol: 'BUSD',
     lpAddresses: {
-      97: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-      56: '',
+      97: '0xe9e7cea3dedca5984780bafc599bd69add087d56', 56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+      
     },
     tokenSymbol: 'BUSD',
     tokenAddresses: {
-      97: '0xe9e7cea3dedca5984780bafc599bd69add087d56', // FIXME:
-      56: '', // https://bscscan.com/address/0xe9e7cea3dedca5984780bafc599bd69add087d56
+      97: '0xe9e7cea3dedca5984780bafc599bd69add087d56', 56: '0xe9e7cea3dedca5984780bafc599bd69add087d56', // FIXME:
+       // https://bscscan.com/address/0xe9e7cea3dedca5984780bafc599bd69add087d56
     },
     quoteTokenSymbol: QuoteToken.BUSD,
     quoteTokenAdresses: contracts.busd,
@@ -180,13 +179,13 @@ const farms: FarmConfig[] = [
     isTokenOnly: true,
     lpSymbol: 'BNB',
     lpAddresses: {
-      97: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
-      56: '',
+      97: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', 56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+      
     },
     tokenSymbol: 'BNB',
     tokenAddresses: {
-      97: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', // FIXME:
-      56: '', // https://bscscan.com/address/0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c
+      97: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', 56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', // FIXME:
+       // https://bscscan.com/address/0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c
     },
     quoteTokenSymbol: QuoteToken.BNB,
     quoteTokenAdresses: contracts.wbnb,
@@ -198,13 +197,13 @@ const farms: FarmConfig[] = [
     isTokenOnly: true,
     lpSymbol: 'CAKE',
     lpAddresses: {
-      97: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
-      56: '', // CAKE-BUSD LP
+      97: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82', 56: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
+       // CAKE-BUSD LP
     },
     tokenSymbol: 'CAKE',
     tokenAddresses: {
-      97: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
-      56: '',
+      97: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82', 56: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
+
     },
     quoteTokenSymbol: QuoteToken.CAKE,
     quoteTokenAdresses: contracts.cake,
@@ -216,13 +215,13 @@ const farms: FarmConfig[] = [
     isTokenOnly: true,
     lpSymbol: 'SIL',
     lpAddresses: {
-      97: '0xc66E4De0d9b4F3CB3f271c37991fE62f154471EB',
-      56: '',
+      97: '0xc66E4De0d9b4F3CB3f271c37991fE62f154471EB', 56: '0xc66E4De0d9b4F3CB3f271c37991fE62f154471EB',
+
     },
     tokenSymbol: 'SIL',
     tokenAddresses: {
-      97: '0xc66E4De0d9b4F3CB3f271c37991fE62f154471EB',
-      56: '',
+      97: '0xc66E4De0d9b4F3CB3f271c37991fE62f154471EB', 56: '0xc66E4De0d9b4F3CB3f271c37991fE62f154471EB',
+
     },
     quoteTokenSymbol: QuoteToken.SIL,
     quoteTokenAdresses: contracts.sil,
@@ -234,13 +233,13 @@ const farms: FarmConfig[] = [
     isTokenOnly: true,
     lpSymbol: 'BSCPAD',
     lpAddresses: {
-      97: '0x5a3010d4d8d3b5fb49f8b6e57fb9e48063f16700',
-      56: '', // DAI-BUSD LP
+      97: '0x5a3010d4d8d3b5fb49f8b6e57fb9e48063f16700', 56: '0x5a3010d4d8d3b5fb49f8b6e57fb9e48063f16700',
+       // DAI-BUSD LP
     },
     tokenSymbol: 'BSCPAD',
     tokenAddresses: {
-      97: '0x5a3010d4d8d3b5fb49f8b6e57fb9e48063f16700',
-      56: '',
+      97: '0x5a3010d4d8d3b5fb49f8b6e57fb9e48063f16700', 56: '0x5a3010d4d8d3b5fb49f8b6e57fb9e48063f16700',
+
     },
     quoteTokenSymbol: QuoteToken.BSCPAD,
     quoteTokenAdresses: contracts.bscpad,
